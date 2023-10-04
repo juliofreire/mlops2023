560f22a2d0aeab2e1e56ec60ce12a5d3d31bf5b8
Júlio Freire <65356482+juliofreire@users.noreply.github.com>
2023-10-04 22:20:54 +0000

    semi-clean code

--- Python_Essentials_for_MLOps/Project_01/movie_recommendations.py
+++ Python_Essentials_for_MLOps/Project_01/movie_recommendations.py
@@ -1,43 +1,44 @@
+"""This code provide a movie recomendation based on what films someone watched."""
+import re
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import ipywidgets as widgets
+from IPython.display import display
 
 # https://files.grouplens.org/datasets/movielens/ml-25m.zip
-# Using pandas to open 
+# Using pandas to open
 movies = pd.read_csv('movies.csv')
 
 movies.head()
 
-import re
-
 def clean_title(title):
+    """ DESCRIPTION"""
     title = re.sub("[^a-zA-Z0-9 ]", "", title)
     return title
 
 movies["clean_title"] = movies["title"].apply(clean_title)
-movies
-
-from sklearn.feature_extraction.text import TfidfVectorizer
+print(movies)
 
 vectorizer = TfidfVectorizer(ngram_range=(1,2))
 
 tfidf = vectorizer.fit_transform(movies["clean_title"])
 
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-
 def search(title):
+    """ DESCRIPTION"""
     title = clean_title(title)
     query_vec = vectorizer.transform([title])
     similarity = cosine_similarity(query_vec, tfidf).flatten()
     indices = np.argpartition(similarity, -5)[-5:]
     results = movies.iloc[indices].iloc[::-1]
-    
+
     return results
 
 #pip install ipywidgets
 #jupyter labextension install @jupyter-widgets/jupyterlab-manager
 
-import ipywidgets as widgets
-from IPython.display import display
+
 
 movie_input = widgets.Text(
     value='Toy Story',
@@ -47,6 +48,7 @@ movie_input = widgets.Text(
 movie_list = widgets.Output()
 
 def on_type(data):
+    """#ODO:description"""
     with movie_list:
         movie_list.clear_output()
         title = data["new"]
@@ -57,44 +59,53 @@ movie_input.observe(on_type, names='value')
 
 
 display(movie_input, movie_list)
-movie_id = 89745
+MOVIE_ID = 89745
 
-#def find_similar_movies(movie_id):
-movie = movies[movies["movieId"] == movie_id]
+# def find_similar_movies(movie_id):
+# movie = movies[movies["movieId"] == movie_id]
 ratings = pd.read_csv("ratings.csv")
 
-print(ratings.dtypes)
+# print(ratings.dtypes)
 
-similar_users = ratings[(ratings["movieId"] == movie_id) & (ratings["rating"] > 4)]["userId"].unique()
-similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) & (ratings["rating"] > 4)]["movieId"]
-similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
+# similar_users = ratings[(ratings["movieId"] == movie_id) &
+#                         (ratings["rating"] > 4)]["userId"].unique()
 
-similar_user_recs = similar_user_recs[similar_user_recs > .10]
-all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
-all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
-rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-rec_percentages.columns = ["similar", "all"]
+# similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
+#                             (ratings["rating"] > 4)]["movieId"]
 
-print(rec_percentages)
+# similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
 
-rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
-rec_percentages = rec_percentages.sort_values("score", ascending=False)
-rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")
+# similar_user_recs = similar_user_recs[similar_user_recs > .10]
+# all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
+# all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
+# rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
+# rec_percentages.columns = ["similar", "all"]
+
+# print(rec_percentages)
+
+# rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+# rec_percentages = rec_percentages.sort_values("score", ascending=False)
+# rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")
 
 def find_similar_movies(movie_id):
-    similar_users = ratings[(ratings["movieId"] == movie_id) & (ratings["rating"] > 4)]["userId"].unique()
-    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) & (ratings["rating"] > 4)]["movieId"]
+    """ DESCRIPTION"""
+    similar_users = ratings[(ratings["movieId"] == movie_id) &
+                            (ratings["rating"] > 4)]["userId"].unique()
+    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
+                                (ratings["rating"] > 4)]["movieId"]
     similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
 
     similar_user_recs = similar_user_recs[similar_user_recs > .10]
-    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
+    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &
+                        (ratings["rating"] > 4)]
     all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
     rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
     rec_percentages.columns = ["similar", "all"]
-    
+
     rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
     rec_percentages = rec_percentages.sort_values("score", ascending=False)
-    return rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")[["score", "title", "genres"]]
+    return rec_percentages.head(10).merge(movies, left_index=True,
+                                        right_on="movieId")[["score", "title", "genres"]]
 
 
 movie_name_input = widgets.Text(
@@ -105,14 +116,14 @@ movie_name_input = widgets.Text(
 
 recommendation_list = widgets.Output()
 
-def on_type(data):
-    with recommendation_list:
-        recommendation_list.clear_output()
-        title = data["new"]
-        if len(title) > 5:
-            results = search(title)
-            movie_id = results.iloc[0]["movieId"]
-            display(find_similar_movies(movie_id))
+# def on_type(data):
+#     with recommendation_list:
+#         recommendation_list.clear_output()
+#         title = data["new"]
+#         if len(title) > 5:
+#             results = search(title)
+#             movie_id = results.iloc[0]["movieId"]
+#             display(find_similar_movies(movie_id))
 
 movie_name_input.observe(on_type, names='value')
 
